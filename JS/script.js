aea8d3b95a15dd55a5f7c69cdea39c627026dbe0
OliverGray03 <Oliver.gray90@outlook.com>
2021-04-28 16:38:03 +0000

    html layout and initial JS, addition only

---
+++ JS/script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+
+    }
+
+    runGame("addition");
+
+})
+
+function runGame(gameType) {
+
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type ${gameType}`);
+        throw `unknown game type ${gameType}, aborting!`
+    }
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textcontent = operand1;
+    document.getElementById("operand2").textcontent = operand2;
+    document.getElementById("operator").textcontent = "+";
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
